c6669f9d72d3bf248b0eae2261c7cb13c5b3779d
tanmoy barman <tanmoybarman@gmail.com>
2025-08-20 18:15:19 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# MedicalChatBotDemo
+# MedicalChatBotDemo  https://www.youtube.com/watch?v=KnoVFU0yCUc&t=1733s
+conda  create -n medibot python=3.13.5 -y
+conda activate medibot
+
+verify - 
+conda env list
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="tanmoy barman",
+    author_email="tanmoybarman@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
